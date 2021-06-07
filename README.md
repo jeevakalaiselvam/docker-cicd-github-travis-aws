6da0e16cab3f71abcca3edd23a603b99b219ae06
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2021-06-07 18:36:25 +0530

    update readme

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 ## This is a simple CICD workflow with Docker
 
 ### React -> Github -> Travis -> AWS
+
+## Command Reference
+
+### Start / Build / Test React application
+
+    npm run start //Start the application
+    npm run test //Run tests for the application
+    npm run build //Generate build for the application
+
+### Build the development image in docker
+
+    docker build -f Dockerfile.dev .
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders application name link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/demo application/i);
+    expect(linkElement).toBeInTheDocument();
 });
